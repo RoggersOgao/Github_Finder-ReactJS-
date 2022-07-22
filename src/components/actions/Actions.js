ea9f518f66d965ef588c45c85b3c13576cc1f369
36178660 <roggersog@gmail.com>
2022-07-22 05:08:09 +0300

    update

--- src/components/actions/Actions.js
+++ src/components/actions/Actions.js
@@ -6,7 +6,7 @@ import axios  from "axios"
 const github = axios.create({
     baseURL:process.env.REACT_APP_GITHUB_URL,
     headers:{
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+        // Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
     }
 })
         //Search for the users
